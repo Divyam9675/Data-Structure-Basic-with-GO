284310f1e064ac4810ed064dc62a5c535d6a1ecb
DIVYAM GUPTA <60549197+Divyam9675@users.noreply.github.com>
2020-11-18 19:26:39 +0530

    Add files via upload

---
+++ Recursion/Recursion3.go
@@ -0,0 +1,20 @@
+package main 
+  
+import ( 
+    "fmt"
+) 
+
+func print_num(n int) { 
+ 
+    if n > 0 { 
+          
+        print_num(n-1) 
+          
+        fmt.Println(n) 
+    } 
+} 
+   
+func main() { 
+      
+    print_num(5) 
+} 
